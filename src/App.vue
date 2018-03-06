2b53e5c4a2396fa91cf48218ed908445167c07b7
김휘겸 <breakstorm00@gmail.com>
2018-03-06 11:42:59 +0900

    Update rest API example (vue에서 rest API 사용 예제)

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,21 @@
     <button>just HTML TAG</button>
     <local-component></local-component>
     <global-component></global-component>
+    <ul v-if='posts && posts.length'>
+      <li v-for='post in posts'>
+        <p><strong>{{ post.title }}</strong></p>
+        <p>{{ post.body }}</p>
+      </li>
+    </ul>
+    <ul v-if='errors && errors.length'>
+      <li v-for='error in errors'>{{ error.message }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import axios from 'axios'
 
 Vue.component('global-component', {
   template: `<div>A global component</div>`
@@ -25,8 +35,19 @@ export default {
   },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      posts: [],
+      error: []
     }
+  },
+  created () {
+    axios.get('http://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e);
+    })
   }
 }
 </script>
