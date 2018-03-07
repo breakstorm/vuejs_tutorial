b6e750a926294b71b7f15e646048943a8c0d4646
김휘겸 <breakstorm00@gmail.com>
2018-03-07 09:59:57 +0900

    Update components props deverli (컴포넌츠 딜리버리)

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
   <div id="app">
     <input type='text' v-model='postBody' v-on:change='postPost()'>
     <button>just HTML TAG</button>
+    <child v-bind:message='postBody'></child>
     <local-component></local-component>
     <global-component></global-component>
+    <div id="counter-event-example">
+      <p>{{ total }}</p>
+      <button-counter v-on:increment='incrementTotal'></button-counter>
+      <button-counter v-on:increment='incrementTotal'></button-counter>
+    </div>
     <ul v-if='errors && errors.length'>
       <li v-for='error in errors'>{{ error.message }}</li>
     </ul>
@@ -29,18 +35,36 @@ var cmp = {
   template: `<div>A local component</div>`
 }
 
+Vue.component('child', {
+  props: ['message'],
+  template: '<strong>hello world direct {{ message }}</strong>'
+})
 
+Vue.component('button-counter', {
+  template: '<button v-on:click="incrementCounter">{{ counter }}</button>',
+  data: () => ({
+      counter: 0
+  }),
+  methods: {
+    incrementCounter: function () {
+      this.count += 1;
+      this.$emit('increment');
+    }
+  }
+})
 export default {
   name: 'app',
   components: {
-    'local-component': cmp
+    'local-component': cmp,
   },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
       posts: [],
       errors: [],
-      postBody: ''
+      postBody: '',
+      message: 'hello world',
+      total: 0
     }
   },
   created () {
@@ -57,6 +81,14 @@ export default {
     .catch(e => {
       this.errors.push(e);
     })
+    // this.posts = [{"userId":102,"id":102,"title":"aaa","body":"bbb"}] 
+  },
+  mounted () {
+    console.log('what')
+    // debugger;
+    this.posts = [{"userId":102,"id":102,"title":"sunt aut facere repellat provident occaecati excepturi optio reprehenderit","body":"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"}]
+      
+      // {"userId":102,"id":102,"title":"sunt aut facere repellat provident occaecati excepturi optio reprehenderit","body":"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"}
   },
   methods: {
     // async postPost () {
@@ -68,15 +100,22 @@ export default {
     //   console.log(response)
     // }
     postPost() {
-      axios.post(`http://jsonplaceholder.typicode.com/posts`, {
-        body: this.postBody
-      })
-      .then(response => {
-        console.log(response)
-      })
-      .catch(e => {
-        this.errors.push(e)
-      })
+      // axios.post(`http://jsonplaceholder.typicode.com/posts`, {
+      //   body: this.postBody
+      // })
+      // .then(response => {
+      //   console.log(response)
+      // })
+      // .catch(e => {
+      //   this.errors.push(e)
+      // })
+
+      // this.posts = this.posts.push({"userId":102,"id":102,"title":"aaa","body":"bbb"});
+      // this.posts = this.posts.concat({"userId":102,"id":102,"title":"aaa","body":"bbb"})
+      this.posts = this.posts.push({"userId":102,"id":102,"title":"aaa","body":"bbb"})
+    },
+    incrementTotal: function () {
+      this.total += 1
     }
   }
 }
